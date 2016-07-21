c63ab95c430f16d000cd005f7cb86c97b7c87050
talvbansal <talvbansal@hotmail.com>
2016-07-21 16:10:23 +0100

    added a new test to create a post, unsure if this way is better

--- tests/Acceptance/BlogPostTest.php
+++ tests/Acceptance/BlogPostTest.php
@@ -33,7 +33,38 @@ class BlogPostTest extends TestCase
      */
     private function createPostData()
     {
-        return factory( \Easel\Models\Post::class )->make(['content_raw' => 'test content']);
+        return factory(\Easel\Models\Post::class)->make([ 'content_raw' => 'test content' ]);
+    }
+
+    /**
+     * Not sure if this or the following test is better for some reason in this test we can't check whats in session.
+     * However this hits the page with the form on too which is better than just the back end code being tested
+     */
+    public function test_a_user_can_create_a_post()
+    {
+        $post = $this->createPostData();
+
+        $this->actingAs($this->user)->visit('admin/post/create')
+             ->type($post->title, 'title')
+             ->type($post->subtitle, 'subtitle')
+             ->type($post->content_raw, 'content')
+             ->type($post->published_at->format('d/m/Y h:i:s'), 'published_at')
+             ->select($post->layout, 'layout')
+             ->press('Save');
+
+        // Is it there?
+        $this->seeInDatabase('posts', [
+            'title'        => $post->title,
+            'slug'         => $post->slug,
+            'subtitle'     => $post->subtitle,
+            'content_raw'  => $post->content,
+            'content_html' => '<p>' . $post->content . '</p>',
+            'published_at' => $post->published_at->format('Y-m-d h:i:s'),
+            'layout'       => $post->layout
+        ]);
+
+        $this->assertSessionHas('_new-post', trans('easel::messages.create_success', [ 'entity' => 'Post' ]));
+        $this->seePageIs('admin/post');
     }
 
     public function test_a_post_can_be_created()
@@ -71,7 +102,6 @@ class BlogPostTest extends TestCase
         $post = $this->createPostData();
         $post->save();
 
-
         // Edit the post
         $title   = 'Edited Post title';
         $content = 'We have new content!';
@@ -98,7 +128,7 @@ class BlogPostTest extends TestCase
         ]);
 
         $this->assertSessionHas('_update-post', trans('easel::messages.update_success', [ 'entity' => 'Post' ]));
-        $this->assertRedirectedTo('/admin/post/'. $post->id .'/edit');
+        $this->assertRedirectedTo('/admin/post/' . $post->id . '/edit');
     }
 
     public function test_a_post_can_be_deleted()
@@ -107,13 +137,13 @@ class BlogPostTest extends TestCase
         $post = $this->createPostData();
         $post->save();
 
-        $this->assertTrue( \Easel\Models\Post::count() === 1);
+        $this->assertTrue(\Easel\Models\Post::count() === 1);
 
         // Delete it!
-        $this->actingAs($this->user)->delete('admin/post/' . $post->id );
+        $this->actingAs($this->user)->delete('admin/post/' . $post->id);
 
         // Is It There?
-        $this->assertTrue( \Easel\Models\Post::count() === 0);
+        $this->assertTrue(\Easel\Models\Post::count() === 0);
 
         $this->assertSessionHas('_delete-post', trans('easel::messages.delete_success', [ 'entity' => 'Post' ]));
         $this->assertRedirectedTo('/admin/post');
