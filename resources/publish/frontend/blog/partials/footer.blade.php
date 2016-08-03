f0e57c0fd92d25e3b53ef0f6367c83685cdb99fc
talvbansal <talvbansal@hotmail.com>
2016-08-03 14:00:04 +0100

    remove disqus from blog post list page

--- resources/publish/frontend/blog/partials/footer.blade.php
+++ resources/publish/frontend/blog/partials/footer.blade.php
@@ -1,17 +1,4 @@
 <div class="container">
-
-    @if(config('easel.disqus_name'))
-
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                @include('vendor.easel.frontend.blog.partials.disqus')
-            </div>
-        </div>
-
-        <br>
-
-    @endif
-
     <div class="text-center">
         <hr width="50%">
         <span id="subtitle">{{ config('easel.subtitle') }}</span>
--- resources/publish/frontend/blog/post.blade.php
+++ resources/publish/frontend/blog/post.blade.php
@@ -92,4 +92,18 @@
 
     @include('vendor.easel.frontend.blog.partials.author')
 
+
+    @if(config('easel.disqus_name'))
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    @include('vendor.easel.frontend.blog.partials.disqus')
+                </div>
+            </div>
+        </div>
+        <br>
+
+    @endif
+
 @stop
