13977872e412948a2eac5a7a70f1a5d83bafb228
talvbansal <talvbansal@hotmail.com>
2016-08-10 10:46:29 +0100

    test that post exists first before rendering social media networks

--- resources/publish/frontend/blog/partials/header.blade.php
+++ resources/publish/frontend/blog/partials/header.blade.php
@@ -3,7 +3,7 @@
         <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
             <h1><a href="{{url('/')}}">{{ config('easel.title') }}</a></h1>
 
-            @if( $post )
+            @if( isset($post) )
                 @foreach( $post->author->social_media as $network => $url )
                     @if( !empty($url) )
                         <a href="{{ $url }}" target="_blank" class="social">
