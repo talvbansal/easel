85b7c3b74dd6e9725377a84a5526625a647b5031
talvbansal <talvbansal@hotmail.com>
2016-08-16 16:36:33 +0100

    simplify notifications code

--- resources/views/backend/post/partials/notifications/update-post.blade.php
+++ resources/views/backend/post/partials/notifications/update-post.blade.php
@@ -1,36 +1,10 @@
 <div id="_update-post" data-field-id="{{ Session::get('_update-post') }}"></div>
 
-<script type="text/javascript">
+<script>
     $(document).ready(function(){
-        $(window).load(function(){
-            function notify(message, type){
-                $.growl({
-                    message: message
-                },{
-                    type: type,
-                    allow_dismiss: false,
-                    label: 'Cancel',
-                    className: 'btn-xs btn-inverse',
-                    placement: {
-                        from: 'top',
-                        align: 'right'
-                    },
-                    delay: 2500,
-                    animate: {
-                        enter: 'animated fadeInDown',
-                        exit: 'animated fadeOutUp'
-                    },
-                    offset: {
-                        x: 20,
-                        y: 85
-                    }
-                });
-            };
-
-            setTimeout(function () {
-                var message = $('#_update-post').data("field-id");
-                notify(message, 'inverse');
-            }, 300)
-        });
+        setTimeout(function () {
+            var message = $('#_update-post').data("field-id");
+            systemNotification(message, 'inverse');
+        }, 300);
     });
 </script>
--- resources/views/backend/shared/notifications/protip.blade.php
+++ resources/views/backend/shared/notifications/protip.blade.php
@@ -1,34 +1,8 @@
-<script type="text/javascript">
+<script>
     $(document).ready(function(){
-        $(window).load(function(){
-            function notify(message, type){
-                $.growl({
-                    message: message
-                },{
-                    type: type,
-                    allow_dismiss: false,
-                    label: 'Cancel',
-                    className: 'btn-xs btn-inverse',
-                    placement: {
-                        from: 'top',
-                        align: 'right'
-                    },
-                    delay: 4000,
-                    animate: {
-                        enter: 'animated fadeInDown',
-                        exit: 'animated fadeOutUp'
-                    },
-                    offset: {
-                        x: 20,
-                        y: 85
-                    }
-                });
-            };
-
-            setTimeout(function () {
-                var message = '<strong>ProTip!</strong> Use &#8984;+s or CTL+s as keyboard shortcuts to save a form.';
-                notify(message, 'inverse');
-            }, 300)
-        });
+        setTimeout(function () {
+            var message = '<strong>ProTip!</strong> Use &#8984;+s or CTL+s as keyboard shortcuts to save a form.';
+            systemNotification(message, 'inverse');
+        }, 300);
     });
 </script>
