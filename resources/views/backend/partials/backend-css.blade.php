da14f56d26f1dd19ab1171fd73c8c0a96db43a61
talvbansal <talvbansal@hotmail.com>
2017-03-28 19:23:56 +0100

    Re-added font for easel title

--- resources/views/backend/partials/backend-css.blade.php
+++ resources/views/backend/partials/backend-css.blade.php
@@ -1,2 +1,4 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('css/vendor.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/easel.css') }}">
+
+<link href='https://fonts.googleapis.com/css?family=Yellowtail' rel='stylesheet' type='text/css'>
