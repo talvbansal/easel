1b253f2b5b2cc2a15f6255587f2004f515f6c82e
talvbansal <talvbansal@hotmail.com>
2017-08-21 18:20:24 -0400

    Reformat post index table

--- resources/views/backend/post/index.blade.php
+++ resources/views/backend/post/index.blade.php
@@ -45,11 +45,21 @@
                                 <tr>
                                     <th data-column-id="id" data-type="numeric" data-sortable="false">Id</th>
                                     <th data-column-id="title">Title</th>
-                                    <th data-column-id="subtitle">Subtitle</th>
-                                    <th data-column-id="slug">Slug</th>
-                                    <th data-column-id="published" data-type="date" data-order="desc">Published</th>
-                                    <th data-column-id="commands" data-formatter="commands" data-sortable="false">Actions</th>
-                                </tr>
+                                    <th data-column-id="subtitle"
+                                        data-css-class="hidden-xs"
+                                        data-header-css-class="hidden-xs"
+                                    >Subtitle</th>
+                                    <th data-column-id="slug"
+                                        data-css-class="hidden-xs"
+                                        data-header-css-class="hidden-xs"
+                                    >Slug</th>
+                                    <th data-column-id="published" data-formatter="ukdate" data-type="date" data-order="desc">Published</th>
+                                    <th data-column-id="commands"
+                                        data-formatter="commands"
+                                        data-sortable="false"
+                                        data-css-class="text-center"
+                                    >Actions</th>
+                                                                    </tr>
                             </thead>
                             <tbody>
                                 @foreach ($data as $post)
@@ -58,7 +68,7 @@
                                         <td>{{ $post->title }}</td>
                                         <td>{{ $post->subtitle }}</td>
                                         <td>{{ $post->slug }}</td>
-                                        <td>{{ $post->published_at->format('M d, Y') }}</td>
+                                        <td>{{ $post->published_at->format('d/m/Y') }}</td>
                                     </tr>
                                 @endforeach
                             </tbody>
